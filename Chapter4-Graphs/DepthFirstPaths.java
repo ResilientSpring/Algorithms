c6efb150bdd01b2f2723307930d9f787e786f6b2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2025-03-19 01:38:22 +0800

    Committed on or around 2025/03/19

--- Chapter4-Graphs/DepthFirstPaths.java
+++ Chapter4-Graphs/DepthFirstPaths.java
@@ -1,14 +1,12 @@
-
 public class DepthFirstPaths {
 	
 	private boolean marked[];  // Has dfs() been called for this vertex?
 	private int     edgeTo[];  // last vertex on known path to this vertex. 
 	private int     s;
 	
-	
+	public DepthFirstPaths(Graph)
 
 	public static void main(String[] args) {
-		
 		
 	}
 
