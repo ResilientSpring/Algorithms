d6248c32504b2ddfe93269c53bd2b23252c7d475
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2025-03-18 19:33:44 +0800

    Committed on or around 2025/03/18

--- Chapter1-Fundamentals/Bags_Queues_and_Stacks/Page125_Stack.java
+++ Chapter1-Fundamentals/Bags_Queues_and_Stacks/Page125_Stack.java
@@ -1,13 +1,66 @@
 package Bags_Queues_and_Stacks;
 
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'fizzBuzz' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void fizzBuzz(int n) {
+        
+        for(int i = 0 ; i <= n; i++){
+            
+            if( (i % 3) == 0 && (i % 5) == 0){
+                                
+                System.out.println("FizzBuzz");
+                
+            } else if((i % 3) == 0 && (i % 5)!= 0){
+                
+                System.out.println("Fizz");
+                
+            } else if((i % 3) != 0 && (i % 5) == 0){
+                
+                System.out.println("Buzz");
+                
+            } else {
+                
+                System.out.println(i);
+            } 
+            
+        }
+
+    }
+
+}
+
 public class Page125_Stack {
+	
+	
+    public static void main(String[] args) throws IOException {
+    	
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		
-		
-		
-		
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-	}
+        Result.fizzBuzz(n);
+
+        bufferedReader.close();
+    }
 
 }
